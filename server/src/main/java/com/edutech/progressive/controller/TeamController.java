bdf10c362e3c501e2fa87f479961771ec8e129cf
chhayabhandari11 <dummy@gmail.com>
2026-04-11 12:50:08 +0000

    commit

---
+++ server/src/main/java/com/edutech/progressive/controller/TeamController.java
@@ -0,0 +1,61 @@
+package com.edutech.progressive.controller;
+
+import com.edutech.progressive.entity.Team;
+import com.edutech.progressive.service.impl.TeamServiceImplArraylist;
+import com.edutech.progressive.service.impl.TeamServiceImplJdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLException;
+import java.util.List;
+@RestController @RequestMapping("/team")
+public class TeamController {
+    @Autowired
+    TeamServiceImplArraylist teamServiceImplArraylist;
+    TeamServiceImplJdbc teamServiceImplJdbc;
+    public ResponseEntity<List<Team>> getAllTeams() {
+        return null;
+    }
+    
+    @GetMapping("/{teamId}")
+    public ResponseEntity<Team> getTeamById(int teamId) throws SQLException {
+        return  null;
+    }
+    
+    @PostMapping
+    public ResponseEntity<Integer> addTeam(Team team) throws SQLException {
+        return new ResponseEntity<>(teamServiceImplJdbc.addTeam(team),HttpStatus.OK);
+    }
+    @PutMapping("/{teamId}")
+    public ResponseEntity<Void> updateTeam(int teamId, Team team) throws SQLException {
+        return null;
+    }
+
+    @DeleteMapping("/{teamId}")
+    public ResponseEntity<Void> deleteTeam(int teamId) throws SQLException {
+        return null;
+    }
+
+    @GetMapping("/fromArrayList")
+    public ResponseEntity<List<Team>> getAllTeamsFromArrayList() {
+        return new ResponseEntity<>(teamServiceImplArraylist.getAllTeams(),HttpStatus.OK); 
+    }
+    
+    @PostMapping("/toArrayList")
+    public ResponseEntity<Integer> addTeamToArrayList(Team team) {
+        return new ResponseEntity<>(teamServiceImplArraylist.addTeam(team),HttpStatus.CREATED) ;
+    }
+   
+    @GetMapping("/fromArrayList/sorted")
+    public ResponseEntity<List<Team>> getAllTeamsSortedByNameFromArrayList() {
+        return new ResponseEntity<>(teamServiceImplArraylist.getAllTeamsSortedByName(),HttpStatus.OK) ;
+    }
+}
---
+++ server/src/main/java/com/edutech/progressive/entity/Cricketer.java
@@ -0,0 +1,118 @@
+package com.edutech.progressive.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CollectionId;
+
+@Entity
+@Table(name="cricketer")
+public class Cricketer implements Comparable<Cricketer> {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+@Column(name = "cricketer_id")
+private int cricketerId;
+@Column(name="team_id")
+private int teamId;
+@Column(name="cricketer_name")
+private String cricketerName;
+@Column(name="age")
+private int age;
+private String nationality;
+private int experience;
+private String role;
+@Column(name="total_runs")
+private int totalRuns;
+@Column(name="total_wickets")
+private int totalWickets;
+
+public Cricketer() {
+}
+
+
+public Cricketer(int cricketerId, int teamId, String cricketerName, int age, String nationality, int experience,
+    String role, int totalRuns, int totalWickets) {
+    this.cricketerId = cricketerId;
+    this.teamId = teamId;
+    this.cricketerName = cricketerName;
+    this.age = age;
+    this.nationality = nationality;
+    this.experience = experience;
+    this.role = role;
+    this.totalRuns = totalRuns;
+    this.totalWickets = totalWickets;
+}
+
+
+public int getCricketerId() {
+    return cricketerId;
+}
+public void setCricketerId(int cricketerId) {
+    this.cricketerId = cricketerId;
+}
+public int getTeamId() {
+    return teamId;
+}
+public void setTeamId(int teamId) {
+    this.teamId = teamId;
+}
+public String getCricketerName() {
+    return cricketerName;
+}
+public void setCricketerName(String cricketerName) {
+    this.cricketerName = cricketerName;
+}
+public int getExperience() {
+    return experience;
+}
+public void setExperience(int experience) {
+    this.experience = experience;
+}
+public String getRole() {
+    return role;
+}
+public void setRole(String role) {
+    this.role = role;
+}
+public int getTotalRuns() {
+    return totalRuns;
+}
+public void setTotalRuns(int totalRuns) {
+    this.totalRuns = totalRuns;
+}
+public int getTotalWickets() {
+    return totalWickets;
+}
+public void setTotalWickets(int totalWickets) {
+    this.totalWickets = totalWickets;
+}
+public int getAge() {
+    return age;
+}
+
+public void setAge(int age) {
+    this.age = age;
+}
+
+
+public String getNationality() {
+    return nationality;
+}
+
+
+public void setNationality(String nationality) {
+    this.nationality = nationality;
+}
+
+
+@Override
+public int compareTo(Cricketer Obj) {
+    return Integer.compare(this.getExperience(),Obj.getExperience());
+}
+
+
+}
